7260c693ce1249c7729deeb08fd9e233840f4a82
gardahck <semyaza@hotmail.com>
2024-08-27 14:15:42 -0400

    - added dark mode

--- README.md
+++ README.md
@@ -12,5 +12,26 @@ This extension includes:
 * swipe left to open the side bar starting right of center
 * swipe right to open header menu starting left of center
 
+## 2.3 
+
+* minor link fix
+
+## 2.2
+
+* fix issue with video width
+* fix some comment page layout issues
+* fix footer width issue
+
+## 2.1 
+
+* fix issue in comments for text to take 100% width
+* other minor layout and colour changes
+
+## 2.0
+
+* added dark mode support. New styles/layout when dark mode is enabled.
+* fix for video expanding past 100%
+* fix for submenu when expanding into logo (now scrollable if needed)
+* fix listing layout to maximize usage of space
 
 ## this app does not require any special permissions at this time
--- oldReddit.js
+++ oldReddit.js
@@ -4,12 +4,29 @@ const IFRAME = document.querySelector('iframe');
 const VP_CONTENT = 'width=device-width,initial-scale=1, maximum-scale=1';
 const MENU = document.querySelector('body #header .sr-list');
 const SIDEBAR = document.querySelector('body>.side');
+const ENTRIES = document.querySelectorAll('body .content .sitetable .thing');
 
 let xStart = 0;
 
 if(VIEWPORT.getAttribute('content') !== VP_CONTENT)
     VIEWPORT.setAttribute('content', VP_CONTENT)
-VIEWPORT.setAttribute('content', 'width=device-width,initial-scale=1')
+if (window.location.href.indexOf("/comments/") > -1) {
+    VIEWPORT.setAttribute('content', 'width=device-width,initial-scale=1')
+    document.querySelector('.thing .thumbnail').style.display ='none';
+}
+
+ENTRIES.forEach(entry => {
+    const spacer = document.createElement('img');
+    const spacerHeight = entry.querySelector('.thumbnail').clientHeight === 0 ? 50 : entry.querySelector('.thumbnail').clientHeight;
+    spacer.classList.add('imgwrap');
+    spacer.setAttribute('style', `height:${spacerHeight}px`);
+    spacer.setAttribute('align', 'left');
+    entry.querySelector('.entry').prepend(spacer);
+    if (window.location.href.indexOf("/comments/") > -1) {
+        entry.querySelector('.thumbnail').style.display ='none';
+        entry.querySelector('.entry').setAttribute('style', `margin-left:0`);
+    }
+});
 
 IFRAME.setAttribute('width', '100%')
 
